cd1a44f2c6e9fe0837f644396d2121a1705645f7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:57 -0500

    xml commenting

--- src/Soenneker.Unified.HttpClients/UnifiedOpenApiHttpClient.cs
+++ src/Soenneker.Unified.HttpClients/UnifiedOpenApiHttpClient.cs
@@ -45,11 +45,18 @@ public sealed class UnifiedOpenApiHttpClient : IUnifiedOpenApiHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(UnifiedOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(UnifiedOpenApiHttpClient));
